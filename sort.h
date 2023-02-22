b0736644aaa4f2ae7b496f21c6ec175ec1eb869e
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-02-22 09:21:51 +0000

    Task 103 update

--- sort.h
+++ sort.h
@@ -37,5 +37,14 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 int arr_max(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void print_left_right(int *array, int size, int first, int mid);
+void merge(int *array, int size, int first, int mid, int *cpy);
+void mergeSort(int *cpy, int first, int size, int *array);
+void copy_array(int *arr, int *cpy, int size);
 
 #endif /* __SORT__ */
